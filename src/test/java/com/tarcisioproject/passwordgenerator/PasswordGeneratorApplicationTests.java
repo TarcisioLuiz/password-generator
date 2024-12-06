85404886df3520218fd8aa3ba996df8fbbd92808
TarcisioLuiz <tarcisioluizfilho@gmail.com>
2024-12-06 00:12:35 -0300

    estrutura inicial

---
+++ src/test/java/com/tarcisioproject/passwordgenerator/PasswordGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarcisioproject.passwordgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
